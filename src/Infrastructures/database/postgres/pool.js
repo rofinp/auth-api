838a6a76522ba5808cc40a46cdea485a1fe4287c
rofinp <rofinugraha.dev@gmail.com>
2023-12-13 11:08:16 +0700

    fixing typos on pool connection code

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -7,7 +7,7 @@ const testConfig = {
   post: process.env.PGPORT_TEST,
   user: process.env.PGUSER_TEST,
   password: process.env.PGPASSWORD_TEST,
-  database: process.env.PGDATAVASE_TEST,
+  database: process.env.PGDATABASE_TEST,
 };
 
 const prodConfig = {
@@ -15,7 +15,7 @@ const prodConfig = {
   post: process.env.PGPORT,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
-  database: process.env.PGDATAVASE,
+  database: process.env.PGDATABASE,
 };
 
 // const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
