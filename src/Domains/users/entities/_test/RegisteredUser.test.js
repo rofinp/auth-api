d5218bac16a4eaa4d91404dc428db7e8f3f6278c
rofinp <rofinugraha.dev@gmail.com>
2023-12-13 20:01:36 +0700

    create domain entities for users: RegisteredUser.test & RegisteredUser

---
+++ src/Domains/users/entities/_test/RegisteredUser.test.js
@@ -0,0 +1,43 @@
+const RegisteredUser = require('../RegisteredUser');
+
+describe('a RegisteredUser entity', () => {
+  it('should throw an error when the payload does not contain the required properties', () => {
+    // Arrange
+    const payload = {
+      username: 'alditaher',
+      fullname: 'Aldi Taher',
+    };
+
+    // Action & Assert
+    expect(() => new RegisteredUser(payload)).toThrow('REGISTERED_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw an error when the payload does not meet the data type specification', () => {
+    // Arrange
+    const payload = {
+      id: 123,
+      username: 'alditaher',
+      fullname: 'Aldi Taher',
+    };
+
+    // Action & Assert
+    expect(() => new RegisteredUser(payload)).toThrow('REGISTERED_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create registeredUser object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'user-123',
+      username: 'alditaher',
+      fullname: 'Aldi Taher',
+    };
+
+    // Action
+    const registeredUser = new RegisteredUser(payload);
+
+    // Assert
+    expect(registeredUser.id).toEqual(payload.id);
+    expect(registeredUser.username).toEqual(payload.username);
+    expect(registeredUser.fullname).toEqual(payload.fullname);
+  });
+});
