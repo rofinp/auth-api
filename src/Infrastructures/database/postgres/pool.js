47f425eef41e6fb62f693bbf9f49680821242b17
rofinp <rofinugraha.dev@gmail.com>
2023-12-13 11:06:32 +0700

    create pool connection for postgres

---
+++ src/Infrastructures/database/postgres/pool.js
@@ -0,0 +1,33 @@
+/* istanbul ignore file */
+
+const { Pool } = require('pg');
+
+const testConfig = {
+  host: process.env.PGHOST_TEST,
+  post: process.env.PGPORT_TEST,
+  user: process.env.PGUSER_TEST,
+  password: process.env.PGPASSWORD_TEST,
+  database: process.env.PGDATAVASE_TEST,
+};
+
+const prodConfig = {
+  host: process.env.PGHOST,
+  post: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATAVASE,
+};
+
+// const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
+
+const pool = new Pool(process.env.NODE_ENV === 'test' ? testConfig : prodConfig);
+
+module.exports = pool;
+
+/*
+  PENJELASAN
+
+  Komentar kode istanbul ignore file berfungsi untuk memberitahu Jest bahwa berkas ini (pool.js)
+  tidak perlu diuji. Berkas ini akan diabaikan oleh jest dan tidak akan masuk ke dalam laporan
+  coverage test.
+*/
