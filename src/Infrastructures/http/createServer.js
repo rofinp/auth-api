86c37818968845841ee9965bb0b50da1ad5f0f6d
rofinp <rofinugraha.dev@gmail.com>
2023-12-16 21:23:20 +0700

    refactoring error handling at src/Interfaces/http/api/users/handler

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,4 +1,6 @@
 const Hapi = require('@hapi/hapi');
+const ClientError = require('../../Commons/exceptions/ClientError');
+const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
 const users = require('../../Interfaces/http/api/users');
 
 const createServer = async (container) => {
@@ -13,6 +15,35 @@ const createServer = async (container) => {
       options: { container },
     },
   ]);
+
+  server.ext('onPreResponse', (req, res) => {
+    const { response } = req;
+    if (response instanceof Error) {
+      // if the response is an error, handle it as needed
+      const translatedError = DomainErrorTranslator.translate(response);
+
+      // internal handling of client errors
+      if (translatedError instanceof ClientError) {
+        const newResponse = res.response({
+          status: 'fail',
+          message: translatedError.message,
+        });
+        newResponse.code(translatedError.statusCode);
+        return newResponse;
+      }
+
+      // server error handling as needed
+      const newResponse = res.response({
+        status: 'error',
+        message: 'a failure occurred on our servers',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    //  if it's not an error, continue with the previous response (without intervention)
+    return res.continue;
+  });
 
   return server;
 };
--- src/Interfaces/http/api/users/handler.js
+++ src/Interfaces/http/api/users/handler.js
@@ -8,36 +8,16 @@ class UsersHandler {
   }
 
   async postUserHandler(req, res) {
-    try {
-      const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
-      const addedUser = await addUserUseCase.execute(req.payload);
-      const response = res.response({
-        status: 'success',
-        data: {
-          addedUser,
-        },
-      });
-      response.code(201);
-      return response;
-    } catch (error) {
-      // if error occured
-      const translatedError = DomainErrorTranslator.translate(error);
-      if (translatedError instanceof ClientError) {
-        const response = res.response({
-          status: 'fail',
-          message: translatedError.message,
-        });
-        response.code(translatedError.statusCode);
-        return response;
-      }
-
-      const response = res.response({
-        status: 'error',
-        message: 'a failure occurred on our servers',
-      });
-      response.code(500);
-      return response;
-    }
+    const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
+    const addedUser = await addUserUseCase.execute(req.payload);
+    const response = res.response({
+      status: 'success',
+      data: {
+        addedUser,
+      },
+    });
+    response.code(201);
+    return response;
   }
 }
 
