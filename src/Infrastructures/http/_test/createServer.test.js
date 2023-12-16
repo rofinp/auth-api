522c28a975caaaddaa7720f92767e503b2fb4d70
rofinp <rofinugraha.dev@gmail.com>
2023-12-16 16:36:44 +0700

    adding more tests to createServer.test

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -57,5 +57,74 @@ describe('HTTP Server', () => {
       expect(responseJson.status).toEqual('fail');
       expect(responseJson.message).toEqual('cannot create new user: the required property does not exist');
     });
+
+    it('should respond with a 400 status code when the request payload violates the data type specification', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'alditaher',
+        password: 'alditaher007',
+        fullname: ['Aldi Taher'],
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('cannot create new user: the data type does not meet the specification');
+    });
+
+    it('should respond with a 400 status code when the username exceeds 50 characters', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'alditaheralditaheralditaheralditaheralditaheralditaher',
+        password: 'alditaher007',
+        fullname: 'Aldi Taher',
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('cannot create new user: the username exceeds 50 characters');
+    });
+
+    it('should respond with a 400 status code when the username contains a prohibited character', async () => {
+      // Arrange
+      const requestPayload = {
+        username: '~alditaher~',
+        password: 'alditaher007',
+        fullname: 'Aldi Taher',
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('cannot create new user: the username contains a prohibited character');
+    });
   });
 });
